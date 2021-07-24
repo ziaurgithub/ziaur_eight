7229886d91515110e4c7a78cbdf9937b244c1460
ziaurgithub <ziaur252729@gmail.com>
2021-07-24 09:47:37 +0600

    numer8

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+/*
+var name string = "ziaur"
+
+var name2 , city string = "zahid" , "chittagong"
+
+fmt.Println(name , city , name2)
+*/
+var d rune = 'A'
+var age int = 5
+var result float32 = 3.94
+
+fmt.Println(d , age , result)
+
+
+}
+
+
